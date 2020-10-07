cf97a3ab9dd3609f81893aeadad9cf9a2404149d
Billy-code-ui <70236250+Billy-code-ui@users.noreply.github.com>
2020-10-07 00:30:10 -0500

    index.js

--- index.js
+++ index.js
@@ -10,40 +10,39 @@ const { join } = require('path');
 
 client.commands = new Discord.Collection();
 
-const prefix = '~'
+const prefix = '+'; //You can always change your prefix!
 
 const commandFiles = readdirSync(join(__dirname, 'commands')).filter(file => file.endsWith('.js'));
 
 for(const file of commandFiles) {
-  const command = require(join(__dirname, 'commands', `${file}`));
-  client.commands.set(command.name, command);
+    const command = require(join(__dirname, 'commands', `${file}`));
+    client.commands.set(command.name, command);
 }
 
 client.on('error', console.error);
 
 client.once('ready', () => {
-  console.log(`${client.user.tag} is now online!`)
-  client.user.setActivity('With Code...', {type: 'PLAYING'});
+    console.log(`${client.user.tag} is now online!`)
+    client.user.setActivity('Savage Love', {type: 'LISTENING'})
 });
 
 client.on('message', async message => {
-  if(message.author.bot) return;
-  if(message.channel.type === 'dm') return;
-  
-  if(message.content.startsWith(prefix)) {
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    if(message.author.bot) return;
+    if(message.channel.type === 'dm') return;
     
-    cosnt command = args.shift().toLowerCase();
-    
-    if(!client.commands.has(command)) return;
-    
-    try {
-      client.commands.get(command).run(client, message, args);
-     } catch (error) {
-        console.log(error)
-     }
- }
- 
+    if(message.content.startsWith(prefix)) {
+        const args = message.content.slice(prefix.length).trim().split(/ +/);
+        
+        const command = args.shift().toLowerCase();
+        
+        if(!client.commands.has(command)) return;
+        
+        try {
+            client.commands.get(command).run(client, message, args);
+        } catch (error) {
+            console.log(error)
+        }
+    }
 });
 
-client.login(token);  
+client.login(token)
